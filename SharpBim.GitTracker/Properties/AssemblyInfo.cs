49b70639a08f339dc94fa4dc98392a7cd8196184
mostafa90 <mostafa90@gmail.com>
2026-04-19 18:49:51 +0200

    Updated Images

--- SharpBim.GitTracker/Properties/AssemblyInfo.cs
+++ SharpBim.GitTracker/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.811.0.0")]
-[assembly: AssemblyFileVersion("0.811.0.0")]
+[assembly: AssemblyVersion("0.812.0.0")]
+[assembly: AssemblyFileVersion("0.812.0.0")]
 [assembly: NeutralResourcesLanguage("en-BW")]
 
-[assembly: AssemblyInformationalVersion("0.811.0.0")]
+[assembly: AssemblyInformationalVersion("0.812.0.0")]
